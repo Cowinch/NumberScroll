e99ce1f0567f535e164a06ff64edab1edeb9d1e6
Cole Winchester <cowinch@gmail.com>
2023-08-04 16:30:09 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+const offsetMultiplier=25
+let currentNumber=0
+
+
+function increment(){
+    if(currentNumber<10){
+        currentNumber++
+        updateCount()
+    }
+
+}
+
+function decrement(){
+    if(currentNumber>0){
+        currentNumber--
+        updateCount()
+    }
+}
+
+function updateCount(){
+    document.documentElement.style.setProperty('--offset', `${offsetMultiplier*currentNumber}px`)
+    
+}
